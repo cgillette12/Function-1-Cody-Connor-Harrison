7f60aae690f976f2ec51a4e5db64cdeb0d8fadc6
hhcgit <hhc50331994@gmail.com>
2019-03-25 13:57:09 -0700

    part4

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-function createGreeting(){
-    return "Hi, my name is Chris and I'm 29 years old";
+function createGreeting(name,age){
+    return "Hi, my name is "+name+"and I'm "+age+"years old";
 }
