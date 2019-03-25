4380f85514d0c693722a915161fb29f089c9d078
hhcgit <hhc50331994@gmail.com>
2019-03-25 13:58:32 -0700

    fixed

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+console.log("Hi, my name is Chris and I'm 29 years old");
+function createGreeting(){
+    console.log("Hi, my name is Chris and I'm 29 years old");
+}
+function createGreeting(){
+    return "Hi, my name is Chris and I'm 29 years old";
+}
 function createGreeting(name,age){
     return "Hi, my name is "+name+"and I'm "+age+"years old";
 }
